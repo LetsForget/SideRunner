e5b56af49134c904af7d9858886b303fadd7a01a
Mikhail Kryloff <edaurdgriffin@gmail.com>
2023-11-13 23:10:16 +0500

    Intro added

---
+++ Assets/Scripts/UI/IntroScreen.cs
@@ -0,0 +1,57 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class IntroScreen : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup[] images;
+        [SerializeField] private float fadeOutTime;
+        [SerializeField] private float sceneTransferDelay;
+        
+        private int currentIndex = 0;
+        private bool completed;
+        
+        private void Start()
+        {
+            AppearPicture();
+        }
+
+        private void Update()
+        {
+            if (completed)
+            {
+                return;
+            }
+
+            if (Input.anyKeyDown)
+            {
+                AppearPicture();
+            }
+        }
+
+        private void AppearPicture()
+        {
+            if (completed)
+            {
+                return;
+            }
+            
+            if (currentIndex >= images.Length)
+            {
+                completed = true;
+
+                var sequence = DOTween.Sequence();
+
+                sequence.InsertCallback(fadeOutTime + sceneTransferDelay,
+                    () => SceneManager.LoadSceneAsync("PlayScene"));
+                
+                return;    
+            }
+
+            images[currentIndex].DOFade(1, fadeOutTime);
+            currentIndex++;
+        }
+    }
+}
